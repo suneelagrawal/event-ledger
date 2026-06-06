11e32ec25f4f8412002df3ef11f481517ac3f5a9
suneelagrawal <suneel_agrawal@yahoo.com>
2026-06-06 18:09:55 -0500

    adding empty folders and fix to package name

--- event-gateway/src/main/java/com/example/eventgateway/EventGatewayApplication.java
+++ event-gateway/src/main/java/com/example/eventgateway/EventGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.event_gateway;
+package com.example.eventgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
