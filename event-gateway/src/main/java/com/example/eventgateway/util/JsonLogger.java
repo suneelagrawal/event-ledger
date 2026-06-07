e00f6b7c05ad3c4520517793f3e7a8fb8b5423f0
suneelagrawal <suneel_agrawal@yahoo.com>
2026-06-06 19:33:26 -0500

    added json logger

---
+++ event-gateway/src/main/java/com/example/eventgateway/util/JsonLogger.java
@@ -0,0 +1,17 @@
+public class JsonLogger {
+
+    private static final String SERVICE = "event-gateway";
+
+    public static void info(String traceId, String message) {
+
+        System.out.println(
+                "{"
+                        + "\"timestamp\":\"" + Instant.now() + "\","
+                        + "\"level\":\"INFO\","
+                        + "\"service\":\"" + SERVICE + "\","
+                        + "\"traceId\":\"" + traceId + "\","
+                        + "\"message\":\"" + message + "\""
+                        + "}"
+        );
+    }
+}
